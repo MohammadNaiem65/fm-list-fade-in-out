6e726b41fbda7dee675c8ab2ba4893c4698b8ff7
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-10 20:34:05 +0600

    created simple notification like animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
 	const [list, setList] = useState([1, 2, 3, 4, 5]);
 
 	const handleAddItem = () =>
-		setList((prevList) => [...prevList, prevList[prevList.length - 1] + 1]);
+		setList((prevList) => [
+			...prevList,
+			(prevList[prevList.length - 1]
+				? prevList[prevList.length - 1]
+				: 0) + 1,
+		]);
 
 	const handleRemoveItem = (id) =>
 		setList((prevList) => prevList.filter((li) => li !== id));
@@ -19,18 +25,25 @@ function App() {
 					Add
 				</button>
 				<ul className='w-1/3'>
-					{list.map((li) => (
-						<li
-							key={li}
-							className='w-full p-3 border-b flex justify-between items-center last:border-b-0'>
-							<span>Item: {li}</span>
-							<button
-								className='px-3 p-y2 border rounded'
-								onClick={() => handleRemoveItem(li)}>
-								&times;
-							</button>
-						</li>
-					))}
+					<AnimatePresence>
+						{list.map((li) => (
+							<motion.li
+								key={li}
+								className='w-full p-3 border-b flex justify-between items-center last:border-b-0'
+								layout
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								transition={{ layout: { type: 'spring' } }}>
+								<span>Item: {li}</span>
+								<button
+									className='px-3 p-y2 border rounded'
+									onClick={() => handleRemoveItem(li)}>
+									&times;
+								</button>
+							</motion.li>
+						))}
+					</AnimatePresence>
 				</ul>
 			</div>
 		</div>
